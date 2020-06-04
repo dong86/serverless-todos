5785efd04078dbb42990f51dd73747db8a05ed60
djh <djh@djhdeMacBook-Pro.local>
2020-06-05 00:42:39 +0800

    update the serverless.yml

--- src/utils/dynamodb.js
+++ src/utils/dynamodb.js
@@ -3,9 +3,14 @@
 const AWS = require('aws-sdk');
 // 实例化DynamoDB客户端
 // 开发环境下选择localhost
-const dynamodb = new AWS.DynamoDB.DocumentClient({
-  region: 'localhost',
-  endpoint: 'http://localhost:8000'
-});
+// 线上使用IAM role
+let options = {};
+if(process.env.IS_OFFLINE) {
+  options = {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  }
+}
+const dynamodb = new AWS.DynamoDB.DocumentClient(options);
 
 module.exports = dynamodb;
