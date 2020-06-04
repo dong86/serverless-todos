7cda5988f911ea5949e5ea2cd8444c1eeb37d847
djh <djh@djhdeMacBook-Pro.local>
2020-06-04 20:28:34 +0800

    delete the todo

---
+++ src/handlers/delete.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const dynamodb = require('../utils/dynamodb');
+
+const remove = async(event, context) => {
+  // 路径: /{stage}/todos/{id}
+  // 首先获取路径参数id
+  const id = event.pathParameters.id;
+  try {
+    // 查看DynamoDB文档 
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#delete-property
+    // 使用delete方法删除特定Item
+    await dynamodb.get({
+      TableName: 'TodosTable',
+      Key: { id }
+    }).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Deleted item(${id})`
+      }, null, 2)
+    };
+  } catch(err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err)
+    };
+  }
+};
+
+module.exports.handler = remove;
